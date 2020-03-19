1199d34dc58f57c1eded6af77fdb4fd33871f856
Inexorably <ssantillo@ucla.edu>
2020-03-19 15:26:04 -0700

    Add damping field and spring sphere demo functions (see description)
    Added apticCallbackDampingPlaneFieldDemo() and hapticCallbackSphereDemo() for user reference.
    
    These demo functions can be tested in place of the standard hapticCallback call - change the:
    
    std::thread hapticRenderThread(hapticCallback);
    
    declaration to one of these demo threads to test them.

--- hapticAlt/hapticAlt.cpp
+++ hapticAlt/hapticAlt.cpp
@@ -1,7 +1,129 @@
 #include "hdSub.h"
 
+/*****************************************The primary haptic callback thread.************************************************************************/
+/*
+* In most cases, the user should make their edits / custom rendering function in this function.
+* This function runs at approximately 100 hz.  Faster frequencies cause instabilities for integrating 
+* the force for applying psuedo haptic feedback (toggleable) to the mouse.
+*/
+
 void hapticCallback() {
 	while(true) {
+		// Get the position of the device.
+		hduVector3Dd position;
+		hdGetDoublev(HD_CURRENT_POSITION, position);
+
+		// you don't have to use the following variables, but they may be useful
+		hduVector3Dd normal(0, 0, 0);
+		hduVector3Dd f(0, 0, 0);
+		double  dist = 0;
+
+		//*** START EDITING HERE ***//////////////////////////////////////////////////
+		//////////////////////////////////////////////////////////////////////////////
+
+		////////////////////////////////////////////////////Render a 3d rigid sphere//////////////////////////////
+		//Define the center in x y z of the sphere.
+		//Define the sphere radius.  Is there an advanrage to using the HDdouble over standard c++ types?
+		hduVector3Dd sphereCenter(900, 500, 0);
+		HDdouble sphereRadius = 250;
+		HDdouble k = 1200;	//N/mm.
+
+		//The distance between position and the sphereCenter.  Note that pow might not be defined for HDdouble type...
+		hduVector3Dd distanceVector = position - sphereCenter;
+		HDdouble distance = distanceVector.magnitude();
+
+		//If distance is less than sphereRadius, we are inside the sphere.
+		if (distance < sphereRadius) {
+			//We find the unit vector from the center of the sphere to the HIP position.
+			hduVector3Dd rHat = position - sphereCenter;
+			//Normalize the vector.
+			rHat.normalize();
+
+			//Set f.  May have to static cast to double for distanceSquared.  Note that this is still optimal because we
+			//don't need to do the sqrt operation in non collision cases.
+			f = k * (sphereRadius - distance) * rHat;
+		}
+
+		//Output the haptic feedback.
+		hdSetDoublev(HD_CURRENT_FORCE, f);
+
+		//////////////////////////////////////////////////////////////////////////////////
+		//*** STOP EDITING HERE ***//////////////////////////////////////////////////////
+
+		//We apply some "force" to the mouse, where we do A*dt^2/2 for distance change in x y z.
+		//Note that we treat the "force" as an acceleration -- we do not take into account mass as we just have a mouse for ux.
+
+		//If the force's magnitude that we would apply to the mouse is less than globalTracker.m_stationaryForce, we apply no values.
+		//Please see the declaration of globalTracker.m_stationaryForce in hdSub.h for further explanation of this variable.
+
+		//Check our elapsed time for force integration.
+		LARGE_INTEGER frequency;				// ticks per second
+		static LARGE_INTEGER prev_ticks;		// ticks in the last iteration.
+		LARGE_INTEGER current_ticks;            // ticks in this iteration.
+		double dt;
+
+		// get ticks per second
+		QueryPerformanceFrequency(&frequency);
+		QueryPerformanceCounter(&current_ticks);
+		dt = (current_ticks.QuadPart - prev_ticks.QuadPart) * 1.0 / frequency.QuadPart;
+		prev_ticks = current_ticks;
+
+		//Apply the force based on dt to the cursor.
+		//Get the current cursor position.
+		POINT point;
+		if (!GetCursorPos(&point)) {
+			std::cout << "hapticCallback: Could not get mouse coordinates\n";
+		}
+
+		//Modify the cursor position per the output force f.
+		//This is dependant on what plane we are operating in.
+		//We check which plane we are in and map screen x y to the appropriate indexes.
+		int mapX = -1;
+		int mapY = -1;
+		switch (globalTracker.m_plane) {
+		case XY:
+			mapX = 0;
+			mapY = 1;
+			break;
+		case XZ:
+			mapX = 0;
+			mapY = 2;
+
+			break;
+		case ZY:
+			mapX = 2;
+			mapY = 1;
+
+			break;
+		default:
+			//Catch error fallthroughs.
+			std::cout << "hapticCallback() - globalTracker.m_plane set to incorrect value " << globalTracker.m_plane << std::endl;
+			return;
+		}
+
+		//Integrate the forces (again, treating as acceleration due to no mass info about hip), and find where our new point should be.
+		//We are working in px, so we round to the nearest integer.
+		point.x += std::round(f[mapX] * pow(dt, 2) / 2);
+		point.y += std::round(f[mapY] * pow(dt, 2) / 2);
+
+		//Set the cursor position.
+		if (globalTracker.m_enablePseudoHaptics && !SetCursorPos(point.x, point.y)) {
+			std::cout << "hapticCallback() - Unable to SetCursorPos\n";
+		}
+		
+		//Sleep for increased window for smoother integration.
+		Sleep(10);
+	}
+}
+
+/********************************************Demo haptic Functions*************************************************************/
+
+//A demo function to show a spring modeled plane with damping fields.
+//If the user would like to test this function, this function should replace the hapticCallback function in the call:
+//std::thread hapticRenderThread(hapticCallback);
+//in main.
+void hapticCallbackDampingFieldDemo() {
+	while (true) {
 		// Get the position of the device.
 		hduVector3Dd position;
 		hdGetDoublev(HD_CURRENT_POSITION, position);
@@ -48,10 +170,10 @@ void hapticCallback() {
 
 		//If d is negative, the user is on or in the wall.  If d is positive, the user is outside of the wall.
 		if (d <= 0) {
-			if (int(abs(position[0])) % 300 > 150){
+			if (int(abs(position[0])) % 300 > 150) {
 
 				f.set(-1 * velocity[0] * b, 0, 0);
-					//std::cout << f[0] << ", " << f[1] << ", " << f[2] << "   V :" << velocity.magnitude() << std::endl;
+				//std::cout << f[0] << ", " << f[1] << ", " << f[2] << "   V :" << velocity.magnitude() << std::endl;
 			}
 
 			//The plane exerts normal force on the user.
@@ -124,12 +246,129 @@ void hapticCallback() {
 		if (globalTracker.m_enablePseudoHaptics && !SetCursorPos(point.x, point.y)) {
 			std::cout << "hapticCallback() - Unable to SetCursorPos\n";
 		}
-		
+
 
 		//Sleep for increased window for smoother integration.
 		Sleep(10);
 	}
 }
+
+//A demo function to show a sphere plane modeled as a spring.
+//If the user would like to test this function, this function should replace the hapticCallback function in the call:
+//std::thread hapticRenderThread(hapticCallback);
+//in main.
+void hapticCallbackSpringSphereDemo() {
+	while (true) {
+		// Get the position of the device.
+		hduVector3Dd position;
+		hdGetDoublev(HD_CURRENT_POSITION, position);
+
+		// you don't have to use the following variables, but they may be useful
+		hduVector3Dd normal(0, 0, 0);
+		hduVector3Dd f(0, 0, 0);
+		double  dist = 0;
+
+		//*** START EDITING HERE ***//////////////////////////////////////////////////
+		//////////////////////////////////////////////////////////////////////////////
+
+		////////////////////////////////////////////////////Render a 3d rigid sphere//////////////////////////////
+		//Define the center in x y z of the sphere.
+		//Define the sphere radius.  Is there an advanrage to using the HDdouble over standard c++ types?
+		hduVector3Dd sphereCenter(900, 500, 0);
+		HDdouble sphereRadius = 250;
+		HDdouble k = 900;	//N/mm.
+
+		//The distance between position and the sphereCenter.  Note that pow might not be defined for HDdouble type...
+		hduVector3Dd distanceVector = position - sphereCenter;
+		HDdouble distance = distanceVector.magnitude();
+
+		//If distance is less than sphereRadius, we are inside the sphere.
+		if (distance < sphereRadius) {
+			//We find the unit vector from the center of the sphere to the HIP position.
+			hduVector3Dd rHat = position - sphereCenter;
+			//Normalize the vector.
+			rHat.normalize();
+
+			//Set f.  May have to static cast to double for distanceSquared.  Note that this is still optimal because we
+			//don't need to do the sqrt operation in non collision cases.
+			f = k * (sphereRadius - distance) * rHat;
+		}
+
+		//std::cout << "x: " << position[0] << ", y: " << position[1] << ", z: " << position[2] << ", dist: " << distance << ", magnitude: " << static_cast<double>(f.magnitude()) << std::endl;
+
+		hdSetDoublev(HD_CURRENT_FORCE, f);
+
+		//////////////////////////////////////////////////////////////////////////////////
+		//*** STOP EDITING HERE ***//////////////////////////////////////////////////////
+
+		//We apply some "force" to the mouse, where we do A*dt^2/2 for distance change in x y z.
+		//Note that we treat the "force" as an acceleration -- we do not take into account mass as we just have a mouse for ux.
+
+		//If the force's magnitude that we would apply to the mouse is less than globalTracker.m_stationaryForce, we apply no values.
+		//Please see the declaration of globalTracker.m_stationaryForce in hdSub.h for further explanation of this variable.
+
+		//Check our elapsed time for force integration.
+		LARGE_INTEGER frequency;				// ticks per second
+		static LARGE_INTEGER prev_ticks;		// ticks in the last iteration.
+		LARGE_INTEGER current_ticks;            // ticks in this iteration.
+		double dt;
+
+		// get ticks per second
+		QueryPerformanceFrequency(&frequency);
+		QueryPerformanceCounter(&current_ticks);
+		dt = (current_ticks.QuadPart - prev_ticks.QuadPart) * 1.0 / frequency.QuadPart;
+		prev_ticks = current_ticks;
+
+		//Apply the force based on dt to the cursor.
+		//Get the current cursor position.
+		POINT point;
+		if (!GetCursorPos(&point)) {
+			std::cout << "hapticCallback: Could not get mouse coordinates\n";
+		}
+
+		//Modify the cursor position per the output force f.
+		//This is dependant on what plane we are operating in.
+		//We check which plane we are in and map screen x y to the appropriate indexes.
+		int mapX = -1;
+		int mapY = -1;
+		switch (globalTracker.m_plane) {
+		case XY:
+			mapX = 0;
+			mapY = 1;
+			break;
+		case XZ:
+			mapX = 0;
+			mapY = 2;
+
+			break;
+		case ZY:
+			mapX = 2;
+			mapY = 1;
+
+			break;
+		default:
+			//Catch error fallthroughs.
+			std::cout << "hapticCallback() - globalTracker.m_plane set to incorrect value " << globalTracker.m_plane << std::endl;
+			return;
+		}
+
+		//Integrate the forces (again, treating as acceleration due to no mass info about hip), and find where our new point should be.
+		//We are working in px, so we round to the nearest integer.
+		point.x += std::round(f[mapX] * pow(dt, 2) / 2);
+		point.y += std::round(f[mapY] * pow(dt, 2) / 2);
+
+		//Set the cursor position.
+		if (globalTracker.m_enablePseudoHaptics && !SetCursorPos(point.x, point.y)) {
+			std::cout << "hapticCallback() - Unable to SetCursorPos\n";
+		}
+
+
+		//Sleep for increased window for smoother integration.
+		Sleep(10);
+	}
+}
+
+/********************************************Main*******************************************************************************/
 
 int main(){
 	//Detach and spawn the output (gui rendering) thread.  We are passing stop by reference, so we need to wrap it in std::ref.
@@ -144,5 +383,3 @@ int main(){
 
 	return 1;
 }
-
-//TODO: Add haptic callback function in its own thread.
